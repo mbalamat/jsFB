009f2e5dd132ddc733a16ef19aa066d9d41444bc
mbalamat <mbalamatsias@gmail.com>
2015-04-06 18:23:25 +0300

    Layout changes

--- getAllConversationMessages.js
+++ getAllConversationMessages.js
@@ -1,7 +1,10 @@
-javascript:str="RecMessages<p>~~~~~~~~~Start~~~~~~~~~~~</p>";
+//This script works for the url that is in the following form: facebook.com/messages
+//javascript:
+str=window.location.href;
+str= str.concat("<p>RecMessages</p><p>~~~~~~~~~Start~~~~~~~~~~~</p>");
 links=document.getElementById("webMessengerRecentMessages").getElementsByTagName("p");
 for(i in links){str=str.concat("<p>".concat(links[i].innerHTML))};
 myWin = window.open("", "MsgWindow", "width=500, height=700");
 myWin.document.write(str);
-myWin.document.write("<p>~~~~~~~~~End~~~~~~~~~~~</p>")
+myWin.document.write("<p>~~~~~~~~~End~~~~~~~~~~~</p>");
 //this snippet outputs all loaded messages from a facebook conversation to a new msgBox
